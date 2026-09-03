8a3e86cfd0a85aceef0ef19b97a590581d1ecc31
Dumanski Maxim <dum20031@gmail.com>
2026-09-03 17:03:57 +0300

    buffer_videostream: rewriteFSM (one-hot coding) & refactoring

--- frame_subtraction.srcs/sources_1/new/buffer_videostream.v
+++ frame_subtraction.srcs/sources_1/new/buffer_videostream.v
@@ -46,20 +46,21 @@ module buffer_videostream #(
 );
 
     //Parameters
-    localparam WORDS_PER_LINE = WIDTH_FRAME / (DATA_WIDTH / 8); // 1920/16 = 120
-    localparam PTR_WIDTH = $clog2(WORDS_PER_LINE);             // = 7 (0..127)
+    localparam WORDS_PER_LINE = WIDTH_FRAME / (DATA_WIDTH / 8);
+    localparam PTR_WIDTH = $clog2(WORDS_PER_LINE);
 
     //State FSM
-    reg buf_state;    // 0 = Ping, 1 = Pong
-    reg frame_state;  // 0 = Frame_1, 1 = Frame_2
-    localparam BUF_PING = 1'b0;
-    localparam BUF_PONG = 1'b1;
+    localparam BUF_PING_A = 4'b0001;
+    localparam BUF_PING_B = 4'b0010;
+    localparam BUF_PONG_A = 4'b0100;
+    localparam BUF_PONG_B = 4'b1000;
+    reg [3:0] buf_state;
 
-//BRAM
-(* ram_style = "block" *) reg [DATA_WIDTH-1:0] bufferA_ping [0:15360-1];
-(* ram_style = "block" *) reg [DATA_WIDTH-1:0] bufferA_pong [0:15360-1];
-(* ram_style = "block" *) reg [DATA_WIDTH-1:0] bufferB_ping [0:15360-1];
-(* ram_style = "block" *) reg [DATA_WIDTH-1:0] bufferB_pong [0:15360-1];
+    //BRAM
+    (* ram_style = "block" *) reg [DATA_WIDTH-1:0] bufferA_ping [0:15360-1];
+    (* ram_style = "block" *) reg [DATA_WIDTH-1:0] bufferA_pong [0:15360-1];
+    (* ram_style = "block" *) reg [DATA_WIDTH-1:0] bufferB_ping [0:15360-1];
+    (* ram_style = "block" *) reg [DATA_WIDTH-1:0] bufferB_pong [0:15360-1];
 
     //Pointer write
     reg [PTR_WIDTH-1:0] ptr_wr_a_ping;
@@ -87,80 +88,88 @@ module buffer_videostream #(
     wire [PTR_WIDTH-1:0] waddr_b_pong;
 
     //Write enable assigment
-    assign we_a_ping = buf_wr_en && !buf_wr_full_reg && (buf_state == BUF_PING) && (frame_state == 1'b0);
-    assign we_b_ping = buf_wr_en && !buf_wr_full_reg && (buf_state == BUF_PING) && (frame_state == 1'b1);
-    assign we_a_pong = buf_wr_en && !buf_wr_full_reg && (buf_state == BUF_PONG) && (frame_state == 1'b0);
-    assign we_b_pong = buf_wr_en && !buf_wr_full_reg && (buf_state == BUF_PONG) && (frame_state == 1'b1);
+    assign we_a_ping = buf_wr_en && !buf_wr_full_reg && BUF_PING_A;
+    assign we_b_ping = buf_wr_en && !buf_wr_full_reg && BUF_PING_B;
+    assign we_a_pong = buf_wr_en && !buf_wr_full_reg && BUF_PONG_A;
+    assign we_b_pong = buf_wr_en && !buf_wr_full_reg && BUF_PONG_B;
 
     assign waddr_a_ping = ptr_wr_a_ping;
     assign waddr_b_ping = ptr_wr_b_ping;
     assign waddr_a_pong = ptr_wr_a_pong;
     assign waddr_b_pong = ptr_wr_b_pong;
 
+    //Register temp read from BRAM
+    reg [DATA_WIDTH-1:0] rd_a_ping, rd_b_ping;
+    reg [DATA_WIDTH-1:0] rd_a_pong, rd_b_pong;
+
     //Synth BRAM
     always @(posedge clk) begin
         if (we_a_ping)
             bufferA_ping[waddr_a_ping] <= buf_wr_data;
+        rd_a_ping <= bufferA_ping[ptr_buf_rd];
+
         if (we_b_ping)
             bufferB_ping[waddr_b_ping] <= buf_wr_data;
+        rd_b_ping <= bufferB_ping[ptr_buf_rd];
+
         if (we_a_pong)
             bufferA_pong[waddr_a_pong] <= buf_wr_data;
+        rd_a_pong <= bufferA_pong[ptr_buf_rd];
+
         if (we_b_pong)
             bufferB_pong[waddr_b_pong] <= buf_wr_data;
+        rd_b_pong <= bufferB_pong[ptr_buf_rd];
     end
 
-    //Control pointers
+    //Control write pointers
     always @(posedge clk) begin
         if (!resetn) begin
             ptr_wr_a_ping <= 0;
             ptr_wr_b_ping <= 0;
             ptr_wr_a_pong <= 0;
             ptr_wr_b_pong <= 0;
-            buf_state    <= BUF_PING;
-            frame_state  <= 1'b0;
+            buf_state    <= BUF_PING_A;
             ready_ping   <= 1'b0;
             ready_pong   <= 1'b0;
             buf_wr_full_reg <= 1'b0;
         end else begin
             if (buf_wr_en && !buf_wr_full_reg) begin
-                case ({buf_state, frame_state})
-                    2'b00: begin // Frame 1, Ping
+                case (buf_state)
+                    4'b0001: begin // Frame 1, Ping
                         if (ptr_wr_a_ping == WORDS_PER_LINE - 1) begin
-                            ptr_wr_a_ping <= 0;
-                            frame_state   <= 1'b1;
+                            ptr_wr_a_ping   <= 0;
+                            buf_state       <= BUF_PING_B;
                         end else begin
-                            ptr_wr_a_ping <= ptr_wr_a_ping + 1;
+                            ptr_wr_a_ping   <= ptr_wr_a_ping + 1;
                         end
                     end
 
-                    2'b10: begin // Frame 1, Pong
-                        if (ptr_wr_a_pong == WORDS_PER_LINE - 1) begin
-                            ptr_wr_a_pong <= 0;
-                            frame_state   <= 1'b1;
-                        end else begin
-                            ptr_wr_a_pong <= ptr_wr_a_pong + 1;
-                        end
-                    end
-
-                    2'b01: begin // Frame 2, Ping
+                    4'b0010: begin // Frame 2, Ping
                         if (ptr_wr_b_ping == WORDS_PER_LINE - 1) begin
-                            ptr_wr_b_ping <= 0;
-                            ready_ping    <= 1'b1;
-                            buf_state     <= BUF_PONG;
-                            frame_state   <= 1'b0;
+                            ptr_wr_b_ping   <= 0;
+                            ready_ping      <= 1'b1;
+                            buf_state       <= BUF_PONG_A;
                         end else begin
-                            ptr_wr_b_ping <= ptr_wr_b_ping + 1;
+                            ptr_wr_b_ping   <= ptr_wr_b_ping + 1;
                         end
                     end
 
-                    2'b11: begin // Frame 2, Pong
-                        if (ptr_wr_b_pong == WORDS_PER_LINE - 1) begin
-                            ptr_wr_b_pong <= 0;
-                            ready_pong    <= 1'b1;
-                            buf_state     <= BUF_PING;
-                            frame_state   <= 1'b0;
+                    4'b0100: begin // Frame 1, Pong
+                        if (ptr_wr_a_pong == WORDS_PER_LINE - 1) begin
+                            ptr_wr_a_pong   <= 0;
+                            buf_state       <= BUF_PONG_B;
                         end else begin
-                            ptr_wr_b_pong <= ptr_wr_b_pong + 1;
+                            ptr_wr_a_pong   <= ptr_wr_a_pong + 1;
+                        end
+                    end
+
+                    4'b1000: begin // Frame 2, Pong
+                        if (ptr_wr_b_pong == WORDS_PER_LINE - 1) begin
+                            ptr_wr_b_pong   <= 0;
+                            ready_pong      <= 1'b1;
+                            buf_state       <= BUF_PING_A;
+                        end else begin
+                            ptr_wr_b_pong   <= ptr_wr_b_pong + 1;
                         end
                     end
                 endcase
@@ -175,23 +184,12 @@ module buffer_videostream #(
         end
     end
 
-    //Read from BRAM
-    reg [DATA_WIDTH-1:0] rd_a_ping, rd_b_ping;
-    reg [DATA_WIDTH-1:0] rd_a_pong, rd_b_pong;
-
-    always @(posedge clk) begin
-        rd_a_ping <= bufferA_ping[ptr_buf_rd];
-        rd_b_ping <= bufferB_ping[ptr_buf_rd];
-
-        rd_a_pong <= bufferA_pong[ptr_buf_rd];
-        rd_b_pong <= bufferB_pong[ptr_buf_rd];
-    end
-
-    //Logic read
+    //Register output
     reg [DATA_WIDTH-1:0] rd_data_a_reg;
     reg [DATA_WIDTH-1:0] rd_data_b_reg;
     reg rd_valid_reg;
     
+    //Control read pointers
     always @(posedge clk) begin
         if (!resetn) begin
             rd_data_a_reg <= 0;
@@ -201,26 +199,26 @@ module buffer_videostream #(
         end else begin
             if (rd_en && rd_ready) begin
                 if (ready_ping) begin
-                    rd_data_a_reg <= rd_a_ping;
-                    rd_data_b_reg <= rd_b_ping;
-                    rd_valid_reg  <= 1'b1;
+                    rd_data_a_reg   <= rd_a_ping;
+                    rd_data_b_reg   <= rd_b_ping;
+                    rd_valid_reg    <= 1'b1;
 
                     if (ptr_buf_rd == WORDS_PER_LINE - 1) begin
-                        ptr_buf_rd <= 0;
-                        ready_ping <= 1'b0;
+                        ptr_buf_rd  <= 0;
+                        ready_ping  <= 1'b0;
                     end else begin
-                        ptr_buf_rd <= ptr_buf_rd + 1;
+                        ptr_buf_rd  <= ptr_buf_rd + 1;
                     end
                 end else if (ready_pong) begin
-                    rd_data_a_reg <= rd_a_pong;
-                    rd_data_b_reg <= rd_b_pong;
-                    rd_valid_reg  <= 1'b1;
+                    rd_data_a_reg   <= rd_a_pong;
+                    rd_data_b_reg   <= rd_b_pong;
+                    rd_valid_reg    <= 1'b1;
 
                     if (ptr_buf_rd == WORDS_PER_LINE - 1) begin
-                        ptr_buf_rd <= 0;
-                        ready_pong <= 1'b0;
+                        ptr_buf_rd  <= 0;
+                        ready_pong  <= 1'b0;
                     end else begin
-                        ptr_buf_rd <= ptr_buf_rd + 1;
+                        ptr_buf_rd  <= ptr_buf_rd + 1;
                     end
                 end else begin
                     rd_valid_reg <= 1'b0;
